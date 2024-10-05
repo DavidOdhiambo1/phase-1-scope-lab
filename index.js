49ed67da78616aa4172b38948b801bc69b5d4dba
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-05 16:48:37 +0300

    scope issues

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "peter"
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'bob'
+}
+console.log(leastFavoriteCustomer)
